2555cfd163177954ead707a09511ccef4ec81029
eraydemir-tech <eraydemiree@gmail.com>
2026-06-14 23:58:19 +0300

    İlk yükleme ve sayfaların bağlanması

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["'Cormorant Garamond'", "serif"],
+        sans: ["'DM Sans'", "sans-serif"],
+        mono: ["'DM Mono'", "monospace"]
+      }
+    }
+  },
+  plugins: []
+};
